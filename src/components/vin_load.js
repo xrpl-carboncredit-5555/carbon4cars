7498d32bd0dc647caafa34febedbeb5171733be0
xrpl-carboncredit-5555 <135251325+xrpl-carboncredit-5555@users.noreply.github.com>
2023-06-09 09:48:51 -0600

    Add files via upload

---
+++ src/components/vin_load.js
@@ -0,0 +1,84 @@
+
+const vinLoad = {
+  "loader": [
+        {
+          "vin": "1GTEK19RXVE536195",
+          "properties": {
+            "make":	"GMC",
+            "model":	"Sierra",
+            "year":	"1997",
+            "drivetype":	"4WD/4-Wheel Drive/4x4",
+            "style": "Light Pickup",
+            "engine":	"5.7L",
+            "mfg": "Pontiac, Michigan(USA)",
+            "age": "26 years"
+          }
+        },
+        {
+          "vin": "1C4NJPBA1CD661292",
+          "properties": {
+            "make":	"Jeep",
+            "model":	"Patriot",
+            "year":	"2012",
+            "drivetype":	"FWD/Front-Wheel Drive",
+            "style": "Compact Utility",
+            "engine":	"2.0L",
+            "mfg": "Belvidere, Illinois,(USA)",
+            "age": "11 years"
+          }
+        },
+        {
+          "vin": "3FAFP08Z66R143414",
+          "properties": {
+            "make":	"Ford",
+            "model":	"Fusion",
+            "year":	"2006",
+            "drivetype":	"FWD/Front-Wheel Drive",
+            "style": "4-door sedan, hardtop",
+            "engine":	"2.3L",
+            "mfg": "HERMOSILLO, MEXICO",
+            "age": "17 years"
+          }
+        },
+        {
+          "vin": "1P3BP36D3HF192068",
+          "properties": {
+            "make":	"Plymouth",
+            "model":	"Reliant",
+            "year":	"1987",
+            "drivetype":	"4x2",
+            "style": "4-door sedan, hardtop",
+            "engine":	"2.2L",
+            "mfg": "Newark, Delaware(USA)",
+            "age": "36 years"
+          }
+        },
+        {
+          "vin": "1HD1FAL11NY500561",
+          "properties": {
+            "make":	"Harley Davidson",
+            "model":	"FLHS",
+            "year":	"1992",
+            "drivetype":	"4x2",
+            "style": "Two Wheel Motorcycle",
+            "engine":	"1.34L",
+            "mfg": "York, Pennsylvania(USA)",
+            "age": "31 years"
+          }
+        },
+        {
+          "vin": "JH4KA4630LC007479",
+          "properties": {
+            "make":	"Acura",
+            "model":	"Legend",
+            "year":	"1990",
+            "drivetype":	"4x2",
+            "style": "4-door sedan, hardtop",
+            "engine":	"2.7L",
+            "mfg": "Saitama, Japan",
+            "age": "33 years"
+          }
+        }
+    ]
+}
+export default vinLoad;
